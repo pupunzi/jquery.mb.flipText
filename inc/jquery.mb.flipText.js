a60bb1cdfefb287eed8518c3c943792e37733969
pupunzi <matteo@open-lab.com>
2010-08-16 20:11:27 +0200

    1.9.0

--- inc/jquery.mb.flipText.js
+++ inc/jquery.mb.flipText.js
@@ -1,7 +1,7 @@
 /*******************************************************************************
  jquery.mb.components
  Copyright (c) 2001-2010. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
- email: info@pupunzi.com
+ email: mbicocchi@open-lab.com
  site: http://pupunzi.com
 
  Licences: MIT, GPL
@@ -22,20 +22,22 @@
     return this.each(function(){
       var me   = $(this);
       var html = me.text();
-      me.text(html.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/'/g, escape("'")).replace(/"/g,escape('"')));
+      //      me.text(html.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/'/g, escape("'")).replace(/"/g,escape('"')));
+      me.text(html.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/'/g, "’").replace(/"/g,"“"));
     });
   };
   $.mbflipText= {
     author:"Matteo Bicocchi",
     version:"1.1",
-    flipText:function(tb){
+    flipText:function(tb, dir){
+      if (!dir) dir=false;
       var UTF8encoded=$("meta[http-equiv=Content-Type]").attr("content") && $("meta[http-equiv=Content-Type]").attr("content").indexOf("utf-8")>-1;
       return this.each(function(){
         var el= $(this);
         var h="";
         var w="";
 
-        if(!isIE && !UTF8encoded) el.encHTML();
+        if(!isIE) el.encHTML();
         var txt= el.html();
         var label="";
         var bgcol=(el.css("background-color") && el.css("background-color") != "rgba(0, 0, 0, 0)") ? el.css("background-color"):"#fff";
@@ -54,9 +56,13 @@
           var rot="-90";
           var ta="end";
           var xFix=0;
+          var textDir= dir?"writing-mode:rl;":"";
+          textDir= ""; //todo RL direction
           var yFix=$.browser.opera ? parseInt(w)-(parseInt(w)/4): $.browser.safari?0:0;
           if (tb){
+            yFix=$.browser.opera?20:0;
             xFix= $.browser.safari?(fontsize/4):0;
+            xFix= dir? xFix-20:xFix;
             rot="90, "+((parseInt(w)/2)-xFix)+", "+parseInt(w)/2;
             ta="start";
           }
@@ -66,14 +72,14 @@
           label=$("<object class='flip_label' style='height:"+h+"; width:"+w+";' type='image/svg+xml' data='data:image/svg+xml; charset=utf-8 ," +
                   "<svg xmlns=\"http://www.w3.org/2000/svg\">" +
                   "<rect x=\"0\" y=\"0\" width=\""+w+"\" height=\""+h+"\" fill=\""+bgcol+"\" stroke=\"none\"/>"+
-                  "<text  x=\"-"+xFix+"\" y=\""+yFix+"\" font-family=\""+fontfamily+"\"  fill=\""+fontcolor+"\" font-size=\""+fontsize+"\"  style=\"text-anchor: "+ta+"; " +
+                  "<text  x=\"-"+xFix+"\" y=\""+yFix+"\" font-family=\""+fontfamily+"\"  fill=\""+fontcolor+"\" font-size=\""+fontsize+"\"  style=\"text-anchor: "+ta+"; "+textDir+" " +
                   "dominant-baseline: hanging\" transform=\"rotate("+rot+")\" text-rendering=\"optimizeSpeed\">"+txt+"</text></svg>'></object>" +
                   clickScript +
                   "");
         }
         var wrapper= onClick ? $("<div/>").css("position","relative"): $("");
         var cssPos= el.wrap(wrapper).css("position")!="absolute" || el.css("position")!="fixed"  ?"relative" : el.css("position");
-//        var cssPos= el.css("position")!="absolute" || el.css("position")!="fixed"  ?"relative" : el.css("position");
+        //        var cssPos= el.css("position")!="absolute" || el.css("position")!="fixed"  ?"relative" : el.css("position");
 
         el.html(label).css({position:cssPos, width:w});
       });
